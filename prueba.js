ef79a679cb1d9a2d147f9e4220ae99d1f5f2b593
eduardorram <eduardecomex137@gmail.com>
2021-03-25 16:13:34 -0600

    Tareas de el primer parcial de juanito

---
+++ prueba.js
@@ -0,0 +1,4 @@
+//c 1er ejercicio de java script 
+var Mes = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"]
+alert(Mes)
+// 2do ejercicio
