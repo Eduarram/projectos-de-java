3eac0ba6097f97736089514118bcfdaa3a23ec2e
eduardorram <eduardecomex137@gmail.com>
2021-04-24 20:04:45 -0500

    tareas nuevas del 24-04-2021

--- prueba.js
+++
@@ -1,4 +0,0 @@
-//c 1er ejercicio de java script 
-var Mes = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"]
-alert(Mes)
-// 2do ejercicio
